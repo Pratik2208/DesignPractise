4c4fd0a147990f3a476336cc6b2e22c3f675a94f
Kalpit Shah <KShah@corp.mm-software.com>
2024-02-02 14:56:48 +0530

    added email and password field

--- DesignPractise/Pages/MainPage.xaml.cs
+++ DesignPractise/Pages/MainPage.xaml.cs
@@ -18,6 +18,26 @@
             img.IsVisible = false;
             img2.IsVisible = true;
         }
+
+        private void Entry_Focused(object sender, FocusEventArgs e)
+        {
+            bemail.StrokeThickness = 6;
+        }
+
+        private void Entry_Focused_1(object sender, FocusEventArgs e)
+        {
+            
+            bpassword.StrokeThickness = 6;  
+        }
+
+        private void Entry_Unfocused(object sender, FocusEventArgs e)
+        {
+            bemail.StrokeThickness = 3;
+        }
+        private void Entry_Unfocused_1(object sender, FocusEventArgs e)
+        {
+            bpassword.StrokeThickness=3;
+        }
     }
 
 }
