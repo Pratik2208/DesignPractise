8b911daf8a54e57b4fc53a9af5b5fa4b64ea7fef
Pratik Panchal <ppl@mm-software.com>
2024-02-02 09:49:46 +0530

    Add project files.

---
+++ DesignPractise/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+using DesignPractise.Pages;
+
+namespace DesignPractise
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkit();
+#if DEBUG
+            builder.Logging.AddDebug();
+            builder.Services.AddTransient<MainPage>();
+            
+#endif
+            return builder.Build();
+        }
+    }
+}
---
+++ DesignPractise/Pages/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace DesignPractise.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            img2.IsVisible = false;
+            img.IsVisible = true;
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
+        {
+            img.IsVisible = false;
+            img2.IsVisible = true;
+        }
+    }
+
+}
